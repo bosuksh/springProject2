ddf182cf9d54082ccd1f9a94e1240efc11f716a1
Lee SangHyup <bosuksh@gmail.com>
2019-10-19 22:53:49 +0900

    Spring Project Part2 First Commit

---
+++ eatgo-api/src/test/java/kr/co/springExample2/eatgo/EatgoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.springExample2.eatgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
