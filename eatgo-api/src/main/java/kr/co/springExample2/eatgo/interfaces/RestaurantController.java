49c26d73084b08b21a10a3111341671a4d5bdb2e
Lee SangHyup <bosuksh@gmail.com>
2019-10-21 20:07:24 +0900

    Rest API GET

---
+++ eatgo-api/src/main/java/kr/co/springExample2/eatgo/interfaces/RestaurantController.java
@@ -0,0 +1,36 @@
+package kr.co.springExample2.eatgo.interfaces;
+
+import kr.co.springExample2.eatgo.domain.Restaurant;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class RestaurantController {
+
+    @GetMapping("/restaurants")
+    public List<Restaurant> list() {
+        List<Restaurant> restaurants = new ArrayList<Restaurant>();
+        Restaurant restaurant = new Restaurant(1004L,"Bob zip", "Seoul");
+
+        restaurants.add(restaurant);
+        return restaurants;
+    }
+
+    @GetMapping("/restaurants/{id}")
+    public Restaurant detail(@PathVariable Long id) {
+        List<Restaurant> restaurants = new ArrayList<>();
+        restaurants.add(new Restaurant(1004L,"Bob zip","Seoul"));
+        restaurants.add(new Restaurant(2020L, "Cyber Food", "Seoul"));
+        Restaurant restaurant = restaurants.stream()
+                .filter(restaurant1 -> restaurant1.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+
+        return restaurant;
+    }
+
+}
