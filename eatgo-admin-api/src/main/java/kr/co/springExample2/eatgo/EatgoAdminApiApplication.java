03e1f4edb5cd272103426baba2e87fc6fe069ace
Lee SangHyup <bosuksh@gmail.com>
2019-10-29 23:09:39 +0900

    try to apply application.properties

--- eatgo-admin-api/src/main/java/kr/co/springExample2/eatgo/EatgoAdminApiApplication.java
+++ eatgo-admin-api/src/main/java/kr/co/springExample2/eatgo/EatgoAdminApiApplication.java
@@ -1,12 +1,18 @@
 package kr.co.springExample2.eatgo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class EatgoAdminApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EatgoAdminApiApplication.class, args);
 	}
 
