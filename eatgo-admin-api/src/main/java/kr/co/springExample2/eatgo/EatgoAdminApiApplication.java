79efea2e610f0275a1b62ee74f2bce58ce67f19d
Lee SangHyup <bosuksh@gmail.com>
2019-10-29 16:01:12 +0900

    Problem

--- eatgo-admin-api/src/main/java/kr/co/springExample2/eatgo/EatgoAdminApiApplication.java
+++ eatgo-admin-api/src/main/java/kr/co/springExample2/eatgo/EatgoAdminApiApplication.java
@@ -2,7 +2,9 @@ package kr.co.springExample2.eatgo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+//@PropertySource(value = {"application.yml"})
 @SpringBootApplication
 public class EatgoAdminApiApplication {
 
