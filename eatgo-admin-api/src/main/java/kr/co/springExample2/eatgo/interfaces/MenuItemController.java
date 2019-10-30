2ebe3f44f0072f7de103fa1dc7fc35c67ee9d098
Lee SangHyup <bosuksh@gmail.com>
2019-10-31 00:49:03 +0900

    DB Perpetuation

--- eatgo-admin-api/src/main/java/kr/co/springExample2/eatgo/interfaces/MenuItemController.java
+++ eatgo-admin-api/src/main/java/kr/co/springExample2/eatgo/interfaces/MenuItemController.java
@@ -3,10 +3,7 @@ package kr.co.springExample2.eatgo.interfaces;
 import kr.co.springExample2.eatgo.application.MenuItemService;
 import kr.co.springExample2.eatgo.domain.MenuItem;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +13,11 @@ public class MenuItemController {
 
     @Autowired
     private MenuItemService menuItemService;
+
+    @GetMapping("/restaurants/{restaurantId}/menuitems")
+    public List<MenuItem> list(@PathVariable Long restaurantId) {
+        return menuItemService.getMenuItems(restaurantId);
+    }
 
     @PutMapping("/restaurants/{id}/menuitems")
     public String bulkUpdate(@PathVariable Long id, @RequestBody List<MenuItem> requests) {
--- eatgo-admin-api/src/test/java/kr/co/springExample2/eatgo/interfaces/MenuItemControllerTest.java
+++ eatgo-admin-api/src/test/java/kr/co/springExample2/eatgo/interfaces/MenuItemControllerTest.java
@@ -1,8 +1,12 @@
 package kr.co.springExample2.eatgo.interfaces;
 
 import kr.co.springExample2.eatgo.application.MenuItemService;
+import kr.co.springExample2.eatgo.domain.MenuItem;
+import kr.co.springExample2.eatgo.domain.MenuItemRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -10,8 +14,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,6 +36,8 @@ public class MenuItemControllerTest {
 
     @MockBean
     private MenuItemService menuItemService;
+
+
     @Test
     public void bulkUpdate() throws Exception {
         mvc.perform(put("/restaurants/1/menuitems")
@@ -34,4 +46,5 @@ public class MenuItemControllerTest {
                 .andExpect(status().isOk());
         verify(menuItemService).bulkUpdate(eq(1L), any());
     }
+
 }
